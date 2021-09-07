0168b2811127c9e03e7864ef2e80498de021c313
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-09-08 00:59:30 +0300

    not ekleme

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,33 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NotDefterimMVC.Data;
 using NotDefterimMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
-namespace NotDefterimMVC.Controllers
+namespace NotDefterim.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            this.db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // giriş yapmış kullanıcı yok ise
+            if (!User.Identity.IsAuthenticated)
+                return View("IndexAnonymous");
+
+
+            // giriş yapmış kullanıcının id'si
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            return View(db.Notes.Where(n => n.AuthorId == userId).ToList());
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Controllers/NotlarController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotDefterimMVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace NotDefterim.Controllers
+{
+    [Authorize]
+    public class NotlarController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public NotlarController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Yeni()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Yeni(Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                note.AuthorId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                db.Notes.Add(note);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
